c175f1aa53919974215e4ef1f5dfb35314197476
codersinol <supun.amarasinghe@gmail.com>
2026-04-14 13:47:23 +0530

    added imersian sandbox

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'cdn.imersian.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.imersian-sandbox.com',
+      },
     ],
   },
 };
